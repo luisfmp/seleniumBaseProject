dadc4bf250a4d3b9036e873ced4f7d1279243d42
lmartinez <lmartinez@switchfly.com>
2020-10-19 17:32:57 -0500

    Creates a simple test running in chrome

---
+++ src/test/java/luisfmp/example/ChromeTest.java
@@ -0,0 +1,42 @@
+package luisfmp.example;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ChromeTest
+{
+    private WebDriver driver;
+
+    @BeforeClass
+    public static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @Before
+    public void setupTest() {
+        driver = new ChromeDriver();
+    }
+
+    @After
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void test() throws InterruptedException {
+        driver.get("https://www.youtube.com/watch?v=9caX__jQZBA");
+        TimeUnit.SECONDS.sleep(3);
+        Assert.assertEquals("Introducción a Selenium con Java - YouTube", driver.getTitle());
+    }
+}
